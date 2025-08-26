532c86cf14bf4be5493a040bddb5700895ec645c
Caryln2399 <97661922+Caryln2399@users.noreply.github.com>
2025-08-26 10:26:26 +0700

    Update run_firefox.sh

--- run_firefox.sh
+++ run_firefox.sh
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash
 
-set -xe
+trigger:
+- master
 
-docker run -t --rm -v "$(pwd)":/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"
+pool:
+  vmImage: 'windows-latest'
+
+steps:
+- task: katalonTask@1
+  displayName: 'Execute Katalon Studio project'
+  inputs:
+    version: '10.3.1'
+    executeArgs: '-retry=0 -testSuitePath="Test Suites/TSMobile" -browserType="Remote" -executionProfile="default" -apiKey="56b36516-6e49-4aa9-b28a-8e6e82aa105a" -orgID=368899'
